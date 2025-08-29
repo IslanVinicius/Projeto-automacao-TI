3427ab602c0d83fcc0b0bbeddc4f5661e6131e58
Islan Vinicius <155023652+IslanVinicius@users.noreply.github.com>
2025-08-29 11:31:46 -0300

    Update ConexãoDB.java
    Excluindo senhas de conexão.

--- src/main/java/org/example/data/ConexãoDB.java
+++ src/main/java/org/example/data/ConexãoDB.java
@@ -9,18 +9,18 @@ public class ConexãoDB {
 
     public static Connection conectar(int loja) throws SQLException {
         String url = " ";
-        String user = "SA";
-        String pass = "ERPM";
+        String user = "";
+        String pass = "";
 
         if(loja == 11){
-             url = "jdbc:sqlserver://192.168.51.198\\SQLEXPRESS;"
-                    + "databaseName=LOJA;"
+             url = "jdbc:sqlserver://;"
+                    + "databaseName=;"
                     + "encrypt=false;"
                     + "trustServerCertificate=true;"
                     + "sslProtocol=TLSv1.2;";
         }else{
-             url = "jdbc:sqlserver://10.10." + loja + ".250\\SQLEXPRESS;"
-                    + "databaseName=LOJA;"
+             url = "jdbc:sqlserver://10.10." + loja + ".250\\;"
+                    + "databaseName=;"
                     + "encrypt=false;"
                     + "trustServerCertificate=true;"
                     + "sslProtocol=TLSv1.2;";
@@ -32,8 +32,8 @@ public class ConexãoDB {
     public static Connection conectar(int loja,int caixa) throws SQLException {
         String user = "SA";
         String pass = "ERPM";
-        String url = "jdbc:sqlserver://10.10." + loja + "."+ caixa +"\\SQLEXPRESS;"
-                + "databaseName=PDV;"
+        String url = "jdbc:sqlserver://10.10." + loja + "."+ caixa +"\\;"
+                + "databaseName=;"
                 + "encrypt=false;"
                 + "trustServerCertificate=true;"
                 + "sslProtocol=TLSv1.2;";
