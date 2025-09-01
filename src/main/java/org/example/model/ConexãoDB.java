5f51466089f9cf060d2318269b860123f31e5d57
Islan Vinicius <155023652+IslanVinicius@users.noreply.github.com>
2025-09-01 11:27:45 -0300

    Update ConexãoDB.java

--- src/main/java/org/example/model/ConexãoDB.java
+++ src/main/java/org/example/model/ConexãoDB.java
@@ -12,13 +12,13 @@ public class ConexãoDB {
         String pass = "";
 
         if(loja == 11){
-             url = "jdbc:sqlserver://;"
+             url = "jdbc:sqlserver:;"
                     + "databaseName=;"
                     + "encrypt=false;"
                     + "trustServerCertificate=true;"
                     + "sslProtocol=TLSv1.2;";
         }else{
-             url = "jdbc:sqlserver://10.10." + loja + ".250\\;"
+             url = "jdbc:sqlserver:;"
                     + "databaseName=;"
                     + "encrypt=false;"
                     + "trustServerCertificate=true;"
@@ -29,9 +29,9 @@ public class ConexãoDB {
     }
 
     public static Connection conectar(int loja,int caixa) throws SQLException {
-        String user = "SA";
-        String pass = "ERPM";
-        String url = "jdbc:sqlserver://10.10." + loja + "."+ caixa +"\\;"
+        String user = "";
+        String pass = "";
+        String url = "";"
                 + "databaseName=;"
                 + "encrypt=false;"
                 + "trustServerCertificate=true;"
